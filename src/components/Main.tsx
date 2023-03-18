08e6e7db0557521a358ace79f792a5a22807d50c
toli444 <toli44777@gmail.com>
2023-03-18 20:49:00 +0100

    add filter

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import useFetch from "../hooks/useFetch";
 
 interface Post {
@@ -11,25 +11,41 @@ interface ReposResponse {
     repositories: Post[]
 }
 
+function getHighlightedText(text: string, highlight: string) {
+    const parts = text.split(new RegExp(`(${highlight})`, "gi"));
+    return parts.map((part: string, index: number) => (
+        <React.Fragment key={index}>
+            {part === highlight ? (
+                <mark>{part}</mark>
+            ) : (
+                part
+            )}
+        </React.Fragment>
+    ));
+}
+
 function Main() {
-    const { response: reposData, error, isLoading } = useFetch<ReposResponse>('./api/repos.json');
+    const {response: reposData, error, isLoading} = useFetch<ReposResponse>('./api/repos.json');
+    const [ownerFilter, setOwnerFilter] = useState('');
 
     return (
         <main className="main">
             <form className="filter-panel">
                 <div className="filter-panel-content">
-                    <input type="text" placeholder="Filter by owner" />
+                    <input type="text" placeholder="Filter by owner" onChange={e => setOwnerFilter(e.target.value)}/>
                 </div>
             </form>
             <div className="results">
                 {isLoading ? "Loading..." : (reposData?.repositories?.length || null) && <ul>
-                    {reposData?.repositories.map(repo => (
+                    {reposData?.repositories.filter(repo => {
+                        return repo.owner.includes(ownerFilter);
+                    }).map(repo => (
                         <li key={repo.name}>
                             <dl className="repo-info">
                                 <dt>Name</dt>
                                 <dd>{repo.name}</dd>
                                 <dt>Owner</dt>
-                                <dd>{repo.owner}</dd>
+                                <dd>{getHighlightedText(repo.owner, ownerFilter)}</dd>
                                 <dt>Languages</dt>
                                 <dd>{repo.languages.join(', ')}</dd>
                             </dl>
